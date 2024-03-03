ea0552b45d8266b06a68a5bed5e193d52145fe94
conhodges-su <129909362+conhodges-su@users.noreply.github.com>
2024-03-02 16:09:00 -0800

    Refactored image_processor.py to not transmit image, added comments

--- image_processor.py
+++ image_processor.py
@@ -1,3 +1,14 @@
+"""
+ImageProcessor class handles image transformations using the OpenCV image 
+library. Supports flip, rotate, greyscale, resizing, and thumbnail images in
+version 1.0.
+
+Author: Connor Hodges
+Date: Februrary-March 2024
+Institution: Seattle University
+Version: 1.0
+"""
+
 import abc
 import cv2
 import grpc
@@ -7,56 +18,89 @@ import uuid
 import math
 import os
 
-
-
 # module constants used for performing command comparisons
 FLIP = "flip"
 ROTATE = "rotate"
 LEFT = "left"
 RIGHT = "right"
-RIGHT_DEGREES = -90
-LEFT_DEGREES = 90
 GREYSCALE = "greyscale"
 RESIZE = "resize"
 THUMBNAIL = "thumbnail"
 HORIZONTAL = "horizontal"
 VERTICAL = "vertical"
+
+# Constants used in comparisons and commands
+RIGHT_DEGREES = -90
+LEFT_DEGREES = 90
 THUMBNAIL_SIZE = 200 
 MAXIMUM_PERCENT = 500
 MINIMUM_PERCENT = -95
-ERROR_400 = 400
+MINIMUM_ROTATION = -10000
+MAXIMUM_ROTATION = 10000
 HORIZ_FLIP = 1
 VERT_FLIP = 0
 DEGREES_MODULO = 360
-CHUNK_SIZE = 64 * 1024 # 64 KiB
-NEW_FILE_INCOMING = "NEW_FILE_INCOMING"
+
+# Error codes
+ERROR_400 = 400
+
 
 def uuid_generator(filetype):
+    """
+    Returns a uuid4 along with the image type
+    """
     return str(str(f"{uuid.uuid4()}.{filetype}"))
 
 class IImageProcessor(abc.ABC):
+    """
+    Abstract class for ImageProcessing. Must inherit from the processImage class
+    to corrrectly inherit from the IImageProcessor.
+    """
     @abc.abstractmethod
     def process_image(self):
         pass
 
 class ImageProcessor(IImageProcessor):
+    """
+    ImageProcessor class inherits from IImageProcessor. Class object is created
+    and iterates through a list of commands. Returns a modified image as well
+    as errors, and thumbnails (if requested).
+    """
     def __init__(self, img, string_cmds, img_type):
+        """
+        Class constructor to set values and convert commands too a list
+        """
         self._img = img
         self._cmds = self._cmds_to_list(string_cmds)
         self._img_type = img_type
         self._new_img = None
-        self._new_thumbnail = None
+        self._new_thumbnail = []
         self._errs = None 
+    
+    def _cmds_to_list(self, string_cmds):
+        """
+        Converts a string into a list based on new lines
+        """
+        rows = string_cmds.split("\n")
+        new_list = []
+        for row in rows:
+            updated = row.split()
+            new_list.append(updated)
+        return new_list
 
     
     def process_image(self):
+        """
+        Processes the image. Iterates through the list of commands and returns
+        the image, thumbnails, and errors to the caller
+        """
         # iterate over all commands
         errs = []
         img = cv2.imread(self._img)
         updated_imgs = dict(img=img,
                             img_name=None,
-                            thumbnail=None,
-                            thumbnail_name=None)
+                            thumbnail=[],
+                            thumbnail_name=[])
         for cmd in self._cmds:
             updated_imgs, errs = self._execute_cmd(updated_imgs, cmd, errs)
         self._errs = errs
@@ -66,25 +110,23 @@ class ImageProcessor(IImageProcessor):
         cv2.imwrite(updated_imgs['img_name'], updated_imgs['img'])
         self._new_img = updated_imgs['img_name']
 
-        # write thumbnail to file, only if present
-        if updated_imgs['thumbnail'] is not None:
-            updated_imgs['thumbnail_name'] = uuid_generator(self._img_type)
-            cv2.imwrite(updated_imgs['thumbnail_name'], updated_imgs['thumbnail'])
-            self._new_thumbnail = updated_imgs['thumbnail_name']
+        # Write thumbnails to file to prepare for transmission 
+        if len(updated_imgs['thumbnail']) != 0:
+            for i in range(len(updated_imgs['thumbnail'])):
+                updated_imgs['thumbnail_name'].append(uuid_generator(self._img_type))
+                cv2.imwrite(updated_imgs['thumbnail_name'][i], updated_imgs['thumbnail'][i])
+                self._new_thumbnail.append(updated_imgs['thumbnail_name'][i])
         
-        return updated_imgs, errs
+        # return image, thumbnails, errors, and type of image for use in server
+        return self._new_img, self._new_thumbnail, self._errs, self._img_type
 
-
-    def _cmds_to_list(self, string_cmds):
-        rows = string_cmds.split("\n")
-        new_list = []
-        for row in rows:
-            updated = row.split()
-            new_list.append(updated)
-        return new_list
-    
 
     def _execute_cmd(self, imgs, cmd, errs):
+        """
+        Processes the first chunk of a command and interprets the command to an
+        associated function. The command is called and the return value is 
+        returned from the call back to _execute_cmd()'s caller.
+        """
         if cmd[0] == FLIP:
             return self._flip_image(imgs, cmd, errs)
         elif cmd[0] == ROTATE:
@@ -105,16 +147,15 @@ class ImageProcessor(IImageProcessor):
         """
         Flips the image over the horizontal or vertical axis
         """
-
-        # get commands from the paramter
-        flip_type = cmd[1]
         try:
+            # get commands from the paramter
+            flip_type = cmd[1]
             if flip_type == HORIZONTAL:
                 flip_cmd = HORIZ_FLIP
             elif flip_type == VERT_FLIP:
                 flip_cmd = VERT_FLIP
             else:
-                raise TypeError(f"{flip_type} invalid command. Usage: <horizontal/vertical>")
+                raise TypeError(f"['{flip_type}'] invalid parameter. Usage: flip <horizontal/vertical>")
             imgs['img'] = cv2.flip(imgs['img'], flip_cmd)
             return imgs, errs
         except TypeError as te:
@@ -128,10 +169,10 @@ class ImageProcessor(IImageProcessor):
         word (left for counterclockwise 90 degrees, right for clockwise 90
         degrees) or with an integer between -10000 and 10000
         """
-        # set degrees to the cmd argument, then test if string command followed 
-        # by casting as an int
-        degrees = cmd[1]
         try:
+            # set degrees to the cmd argument, then test if string command followed 
+            # by casting as an int
+            degrees = cmd[1]
             if degrees == LEFT:
                 rotation = LEFT_DEGREES
             elif degrees == RIGHT:
@@ -142,10 +183,14 @@ class ImageProcessor(IImageProcessor):
                     if cmd[1].startswith("+"):
                         cmd[1] = cmd[1][1:]
                     degrees = int(cmd[1])
+                    self._check_rotation_amt(degrees)
                     rotation = degrees % DEGREES_MODULO if degrees >= 0 else (-1 * ((-1 * degrees) % DEGREES_MODULO))
                     rotation *= -1
                 except ValueError as ve:
-                    errs.append((ERROR_400, f"'{cmd[1]}' is invalid rotation command. Only integers allowed"))
+                    errs.append((ERROR_400, f"'{cmd[1]}' is invalid rotation parameters. Only integers allowed"))
+                    return imgs, errs
+                except Exception as e:
+                    errs.append((ERROR_400, e))
                     return imgs, errs
         except ValueError as ve:
             print(f"Error {ve}: could not read degree rotation, returning original")
@@ -190,10 +235,8 @@ class ImageProcessor(IImageProcessor):
         """
         Resizes an image according to a % scalaing factor provided
         """
-
         # get commands from the paramter
         try:
-            # new_size = (width, height), check dimensions
             percent_change = int(cmd[1])
             self._check_dimension(percent_change)
             print(f"Change: {percent_change}")
@@ -213,13 +256,17 @@ class ImageProcessor(IImageProcessor):
     
 
     def _thumbnail_image(self, imgs, cmd, errs):
-        # TODO: only send the first one
+        """
+        Converts the provided image to a thumbnail of size 200 x 200 pixels. The
+        thumbnail is appended to a list of thumbnails and does not replace any
+        image.
+        """
          # get commands from the paramter
         try:
-            self._check_thumbnail_prescence(imgs)
-            # new_size = (width, height), check dimensions
+            # self._check_thumbnail_prescence(imgs)
             new_size = (int(cmd[1]), int(cmd[2]))
-            imgs['thumbnail'] = cv2.resize(imgs['img'], new_size)
+            # imgs['thumbnail'] = cv2.resize(imgs['img'], new_size)
+            imgs['thumbnail'].append(cv2.resize(imgs['img'], new_size))
         except ValueError as ve:
             print(f"Error {ve}")
             errs.append((ERROR_400, ve))
@@ -232,81 +279,32 @@ class ImageProcessor(IImageProcessor):
             return imgs, errs
     
     def _check_dimension(self, percent):
+        """
+        checks the scaling factor and raises a ValueError if greater than 500%
+        or less than -95%
+        """
         if percent > MAXIMUM_PERCENT:
             raise ValueError("Cannot resize/scale by more than 500%")
         elif percent < MINIMUM_PERCENT:
             raise ValueError("Cannot resize/scale by less than -95%")
     
 
-    def _check_thumbnail_prescence(self, imgs):
-        if imgs['thumbnail'] is not None:
-            raise Exception("'thumbnail' call can only be called one time")
+    def _check_rotation_amt(self, rotation):
+        """
+        checks the rotation amount of a rotation call and returns an exception 
+        if less than -10000 degrees or greater than 10000 degrees
+        """
+        if rotation < MINIMUM_ROTATION:
+            raise Exception("Rotation must be greater than -10001 degrees")
+        if rotation > MAXIMUM_ROTATION:
+            raise Exception("Rotation must be less than 10001 degrees")
     
 
     def _convert_to_factor(self, percent):
+        """
+        Converts a user provided percent scaling to a scaling factor value.
+        """
         if percent >= 0:
             return (percent / 100) + 1
         if percent < 0:
             return 1 - (abs(percent) / 100)
-
-    
-    def transmit_img(self):
-        """
-        ref: https://stackoverflow.com/questions/4566498/what-is-the-idiomatic-way-to-iterate-over-a-binary-file
-        """
-        print("transferring back")
-        
-        # prepare error string
-        err_msg = self.error_string()
-        print(err_msg)
-        # send updated image back to client
-        try:
-            with open(self._new_img,'rb') as f:
-                for chunk in iter(lambda: f.read(CHUNK_SIZE), b''):
-                    # print(chunk)
-                    if not chunk:
-                        break
-                    img_return = image_pb2.ImageReturn(
-                        image_type = self._img_type,
-                        img_chunk_data = chunk,
-                        filename = self._new_img,
-                        errors = err_msg)
-                    yield img_return
-        except Exception as e:
-            print(e)
-        
-        # delete the stored file
-        os.remove(self._new_img)
-        
-        # send back an empty return to indicate next file being sent
-        img_return = image_pb2.ImageReturn(
-                        image_type = "",
-                        img_chunk_data = b"",
-                        filename = NEW_FILE_INCOMING,
-                        errors = err_msg)
-        yield img_return
-
-        # send back thumbnail, if any
-        if self._new_thumbnail is not None:
-            try:
-                with open(self._new_thumbnail,'rb') as f:
-                    for chunk in iter(lambda: f.read(CHUNK_SIZE), b''):
-                        # print(chunk)
-                        if not chunk:
-                            break
-                        img_return = image_pb2.ImageReturn(
-                            image_type = self._img_type,
-                            img_chunk_data = chunk,
-                            filename = self._new_thumbnail,
-                            errors = err_msg)
-                        yield img_return
-            except Exception as e:
-                print(e)
-            # delete the stored file
-            os.remove(self._new_thumbnail)
-    
-    def error_string(self):
-        error_msg = ""
-        for err in self._errs:
-            error_msg += f"{err[0]}, {err[1]}\n"
-        return error_msg
