82769539a94a44717aa1638bc1746f93424cabb7
conhodges-su <129909362+conhodges-su@users.noreply.github.com>
2024-03-02 16:48:56 -0800

    Commented cmd_parser.py

--- cmd_parser.py
+++ cmd_parser.py
@@ -1,20 +1,42 @@
+"""
+The cmd_parser.py file handles the clietn side functionality to interact with
+a gRPC-based server. The file requires that the image_pb2 and image_pb2_grpc
+files are generated and and available to the caller.
+
+Author: Connor Hodges
+Date: Februrary-March 2024
+Institution: Seattle University
+Version: 1.0
+"""
+
+import abc
 import grpc
 import os
 import image_pb2
 import image_pb2_grpc
 
 NEW_FILE_INCOMING = "NEW_FILE_INCOMING"
-IMG_TYPES = ('jpg', 'jpeg', 'png', 'gif', 'tif')
+IMG_TYPES = ('jpg', 'jpeg', 'png', 'tif')
 
-class ICmdParser():
-    def __init__(self, src, cmds, host, port):
-        pass
-
+class ICmdParser(abc.ABC):
+    """
+    Abstract class for CmdParser. Must inherit implement the process_image()
+    method to corrrectly inherit from the ICmdParser.
+    """
+    @abc.abstractmethod
     def process_image(self):
         pass
 
-class CmdParser():
+class CmdParser(ICmdParser):
+    """
+    The CmdParser class processes client side image requests by abstracting away
+    communication with a gRPC-based server stub. Clients create instances of
+    the CmdParser, passing the commands and image files for processing.
+    """
     def __init__(self, src, cmds, host, port):
+        """
+        Constructor for the CmdParser class
+        """
         self.src = src
         self.cmds = cmds
         self.host = host
@@ -23,11 +45,15 @@ class CmdParser():
 
     
     def process_image(self):
+        """
+        Handles communication with an image server usign the host/port provided
+        in the constructor. Image information is transmitted to the server and 
+        results of the processing are collected by process_image and info
+        returned to the client.
+        """
         host_port = self.host + ":" + str(self.port)
         new_img = b""
         new_img_name = b""
-        # new_thumbnail = b""
-        # new_thumbnail_name = b""
         error_msg = b""
         encountered_thumbnail = False
         response_dict = dict(
@@ -110,11 +136,16 @@ class CmdParser():
     
 
     def _check_file_exists(self):
+        """
+        Checks if the image exists, if not a value error is raised
+        """
         if not os.path.isfile(self.src):
             raise ValueError("404, file does not exist")
 
     def _transmit_img(self):
         """
+        Transmits the image to the gRPC-based server using the protobuf 
+        definition.
         ref: https://stackoverflow.com/questions/4566498/what-is-the-idiomatic-way-to-iterate-over-a-binary-file
         """
         str_cmds = "\n".join(self.cmds)
@@ -133,13 +164,21 @@ class CmdParser():
         except Exception as e:
             print(e)
     
+
     def _is_supported_img(self):
+        """
+        Checks the file type of the image to process. if not part of the types
+        supported, false is return. else true.
+        """
         if self._img_type not in IMG_TYPES:
             return False
         return True
     
 
     def _get_image_type(self):
+        """
+        Extracts the image type from the file name
+        """
         chunks = self.src.split('.')
         length = len(chunks)
         # return no image type if no split occurs
@@ -149,6 +188,9 @@ class CmdParser():
     
 
     def convert_to_list(self, str_err):
+        """
+        Converts a string of errors separated by newlines and returns a list.
+        """
         if str_err == "":
             return []
         error_list = str_err.split("\n")
