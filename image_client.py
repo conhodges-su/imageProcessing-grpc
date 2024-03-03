a551a521dcfb243abe4d93d5cd69e67e16a88f67
conhodges-su <129909362+conhodges-su@users.noreply.github.com>
2024-03-02 17:48:13 -0800

    Prepared comments and image display functionality in image_client.py

--- image_client.py
+++ image_client.py
@@ -1,16 +1,48 @@
+"""
+Simple client that uses the CmdParser class to connect to an imageProcessing
+server. Displays the results to the screen.
 
+Author: Connor Hodges
+Date: Februrary-March 2024
+Institution: Seattle University
+Version: 1.0
+"""
+
+import cv2
 import cmd_parser as cmd
 
 FILEPATH = "Nerd_Preview.png"
 if __name__ == "__main__":
-    # execute("screenshot.png")
-    cmd1 = "rotate +40"
+    cmd1 = "rotate 20"
     cmd2 = "thumbnail"
     cmd3 = "greyscale"
-    cmd4 = "thumbnail"
+    cmd4 = "resize 10"
     cmds = [cmd1, cmd2, cmd3, cmd4]
     new_cmd = cmd.CmdParser(FILEPATH, cmds, "localhost", 10760)
     response = new_cmd.process_image()
-    print(response['errors'])
-    print(response['img'])
-    print(response['thumbnail'])
+    
+    print(f"img file: {response['img']}")
+    print(f"thumbnail file: {response['thumbnail']}")
+    print(f"errors: {response['errors']}")
+
+    # print the image to screen
+    # read image 
+    image = cv2.imread(FILEPATH)
+    # show the image, provide window name first
+    cv2.imshow('image window', image)
+    # add wait key. window waits until user presses a key
+    cv2.waitKey(0)
+
+    new_img = cv2.imread(response['img'])
+    cv2.imshow('image window', new_img)
+    # add wait key. window waits until user presses a key
+    cv2.waitKey(0)
+
+    # show the thumbnails
+    for pic in response['thumbnail']:
+        thumb = cv2.imread(pic)
+        cv2.imshow('thumbnail', thumb)
+        cv2.waitKey(0)
+
+    # and finally destroy/close all open windows
+    cv2.destroyAllWindows()
